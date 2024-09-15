575cb75afe378ad29520931236e83174a9233418
Marina Lee <mhlee@usc.edu>
2024-09-15 14:51:11 -0700

    yahtzee dice

--- yahtzee/lib/yahtzeePrep.dart
+++ yahtzee/lib/yahtzeePrep.dart
@@ -2,6 +2,8 @@
 // Your Name Here (replace mine, this is just demos
 // of stuff anyone can use).
 
+import "dart:math";
+
 import "package:flutter/material.dart";
 
 void main() // 23
@@ -30,68 +32,131 @@ class YahtzeeHome extends StatefulWidget
 
 class YahtzeeHomeState extends State<YahtzeeHome>
 {
+  var total = 0;
+  List<Dice> dice = [Dice(), Dice(), Dice(), Dice(), Dice(),];
+
   @override
-  Widget build( BuildContext context )
-  { return Scaffold
-    ( appBar: AppBar(title: const Text("yahtzee")),
-      body: Column
-      ( children:
-        [ Text
-          ( "font styles", 
-            style: TextStyle
-            ( fontSize: 35,
-              color: Colors.orange,
-            ) 
-          ),
-          Spacer(flex:1),
-          Text
-          ( "line 2", 
-            style: TextStyle
-            ( fontWeight: FontWeight.bold,
-              fontSize: 30,
-            ),
-          ),
-          Spacer(flex:1),
-          Container
-          ( decoration: BoxDecoration
-            ( color: Colors.pink,
-              border: Border.all
-              ( width:8,
-              )
-            ),
-            child: Text
-            ( "in box",style:TextStyle
-              ( fontSize: 40)
-            ),
-          ),
-          Container
-          ( decoration: BoxDecoration
-            ( border: Border.all( width:1, ) ),
-            height: 100,
+  Widget build(BuildContext context) { 
+    return Scaffold( 
+      appBar: AppBar(title: const Text("yahtzee")),
+      body:  Column( 
+        children: [ 
+          SizedBox(
             width: 100,
-            child: Stack
-            ( children: 
-              [ Positioned
-                ( left: 10,
-                  top: 40,
-                  child: Text("hi"),
-                ),
-                Text("lo"),
-                Positioned
-                ( left: 80, top: 70,
-                  child: Container
-                  ( height: 10, width: 10,
-                    decoration: BoxDecoration
-                    ( color: Colors.black,
-                      shape: BoxShape.circle,
-                    ),
-                  )
-                ),
-              ],
+            height: 70,
+            child: FloatingActionButton( 
+              onPressed: () { 
+                setState( () { 
+                  total = 0;
+                  for ( Dice d in dice )
+                  { total += d.roll(); } 
+                }
+                );
+              },
+            child: Text("roll all", style: TextStyle(fontSize: 30)),
             ),
           ),
+          Text("total $total", style: TextStyle(fontSize: 30)),
+          Row( children: dice, ),
         ]
       ),
+    );
+  }
+}
+
+class Dot extends Positioned
+{
+  Dot(  {super.top, super.left } )
+  : super
+    ( child: Container
+      ( height: 10, width: 10,
+        decoration: BoxDecoration
+        ( color: Colors.black,
+          shape: BoxShape.circle,
+        ),
+      ),
+    );
+}
+
+class Dice extends StatefulWidget {
+  final DiceState ds = DiceState();
+  @override
+  State<Dice> createState() => ds;
+
+  int roll() { return ds.roll(); }
+
+}
+
+class DiceState extends State<Dice> {
+  var face = 0;
+  bool isHold = false;
+  final rand = Random();
+
+  int roll() {
+    if (!isHold) {
+      setState (
+        () { face = rand.nextInt(6) + 1; }
+      );
+    }
+    return face;
+  }
+
+  Widget build( BuildContext context ) {
+    List<Dot> dots = [];
+    if (face == 1) {
+      dots.add(Dot(top: 40, left: 45)); 
+    }
+    if (face == 2) {
+      dots.add(Dot(top: 10, left: 20));
+      dots.add(Dot(top: 70, left: 70));
+    }
+    if (face == 3) {
+      dots.add(Dot(top: 10, left: 20));
+      dots.add(Dot(top: 70, left: 70));
+      dots.add(Dot(top: 40, left: 45)); 
+    }
+    if (face == 4) {
+      dots.add(Dot(top: 70, left: 20));
+      dots.add(Dot(top: 10, left: 70));
+      dots.add(Dot(top: 10, left: 20));
+      dots.add(Dot(top: 70, left: 70));
+    }
+    if (face == 5) {
+      dots.add(Dot(top: 10, left: 20));
+      dots.add(Dot(top: 70, left: 70));
+      dots.add(Dot(top: 40, left: 45));
+      dots.add(Dot(top: 10, left: 70));
+      dots.add(Dot(top: 70, left: 20));
+    }
+    if (face == 6) {
+      dots.add(Dot(top: 10, left: 20));
+      dots.add(Dot(top: 70, left: 70));
+      dots.add(Dot(top: 10, left: 70));
+      dots.add(Dot(top: 70, left: 20));
+      dots.add(Dot(top: 40, left: 20));
+      dots.add(Dot(top: 40, left: 70));
+    }
+
+    return Column
+    ( children: 
+      [ Container
+        ( decoration: BoxDecoration
+          ( border: Border.all( width:2, ) ,
+            color: (isHold? Colors.pink: Colors.white),
+          ),
+          height: 100, width: 100, 
+          child: Stack( children:dots, ),
+        ),
+        FloatingActionButton
+        ( onPressed: (){ setState((){ isHold = !isHold;}); },
+          child: Text("hold"),
+        ),
+        // just for debugging
+        FloatingActionButton
+        ( onPressed: (){ setState((){ roll(); }); },
+          child: Text("roll"),
+        ),
+      ],
     );
   }
 }
