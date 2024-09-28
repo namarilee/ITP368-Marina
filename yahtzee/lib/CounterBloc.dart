ac9f6eda288211dd9679a20fe9aa5297b1e7822a
Marina Lee <mhlee@usc.edu>
2024-09-28 03:57:29 -0700

    GroceryList lab

--- yahtzee/lib/CounterBloc.dart
+++ yahtzee/lib/CounterBloc.dart
@@ -53,7 +53,7 @@ class MyHomePage extends StatelessWidget {
 							const Text(
 								'You have pushed the button this many times:',
 							),
-							Text(
+							Text( 
 								'${countState.count}',
 								style: Theme.of(context).textTheme.headlineMedium,
 							),
